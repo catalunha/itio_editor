81eae8cb39bb7be217591e648f1d5911a6cc039b
catalunha <catalunha.mj@gmail.com>
2025-03-07 19:15:32 -0300

    web freezed

--- lib/editor/view/editor_default.dart
+++ lib/editor/view/editor_default.dart
@@ -40,7 +40,6 @@ class EditorDefault extends StatelessWidget {
     );
   }
 }
-
 // http://biblingoprod.s3.amazonaws.com/assets/app/badges/Items/Sandals/BadgeSandals.gif
 // https://biblingoprod.s3.amazonaws.com/xmlassets/audio/70.G.1.2.mp4
 // https://biblingoprod.s3.amazonaws.com/assets/app/badges/Items/Sandals/_After.png
